7494a50acb26a1186014a0d976cd97dbf0b1985f
Kydz Liu <boatkyd@gmail.com>
2017-11-18 00:02:23 +0800

    Lay out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from "./side-bar.component";
+import { ArticleListComponent } from "./article-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    ArticleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/article-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.scss']
+})
+
+export class ArticleListComponent {
+}
